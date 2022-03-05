d7da65b9234362f24cd2a7f1849f89b027e1ebf9
Shaish Guni <gunisguni2007@gmail.com>
2022-03-05 20:34:56 +0545

    Final commit before PWA ig

---
+++ weather/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "{427fa29df036c60e44b6b0ca60b18a74}",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
